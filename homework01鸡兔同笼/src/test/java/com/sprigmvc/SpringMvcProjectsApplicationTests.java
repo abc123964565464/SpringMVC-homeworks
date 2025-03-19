586fe0e288858c0a8a0da6f01433a368c7efb51d
zhoujunyuan <2120814280@qq.com>
2025-03-19 22:22:09 +0800

    鸡兔同笼问题

---
+++ homework01鸡兔同笼/src/test/java/com/sprigmvc/SpringMvcProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprigmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
