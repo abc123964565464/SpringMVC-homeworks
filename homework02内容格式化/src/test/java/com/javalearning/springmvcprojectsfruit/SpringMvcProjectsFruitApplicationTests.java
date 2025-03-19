9097a8add46c225066949603ce3b80c37025afe9
zhoujunyuan <2120814280@qq.com>
2025-03-19 22:28:54 +0800

    convert和format 格式化内容

---
+++ homework02内容格式化/src/test/java/com/javalearning/springmvcprojectsfruit/SpringMvcProjectsFruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.springmvcprojectsfruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjectsFruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
